931f7e039c5ca14d6eab1c0753e2a0614855bc5f
zerrisk <zerrisk@gmail.com>
2017-05-17 02:03:10 -0700

    Second revision, clean up ComplexNum class

--- Complex Numbers/ComplexNum.kt
+++ Complex Numbers/ComplexNum.kt
@@ -1,39 +1,42 @@
-class ComplexNum(a: Double = 0.0){
+class ComplexNum(a: Double = 0.0) {
 
-	//Real property
+	// Real property
 	var real: Double
-	//Imaginary property
+	// Imaginary property
 	var imaginary: Double = 0.0
 
-	//primary constructor, initializes the real element
-	init{
+	// Primary constructor, initializes the real element
+	init {
 		real = a
 	}
-	//secondary constructor, initializes the imaginary element if provided
-	constructor(a: Double = 0.0, b: Double) : this(a){
+	
+	// Secondary constructor, initializes the imaginary element if provided
+	constructor(a: Double = 0.0, b: Double) : this(a) {
 		imaginary = b
 	}
+	
+    constructor(a: Number, b: Number) : this(a.toDouble(), b.toDouble())
 
-// higher order function, takes in the "op" function as a parameter and does it on reals and imaginaries
-	private fun doOpOnBoth(other: ComplexNum, op: Double.(Double) -> Double): ComplexNum{
+    // Higher order function, takes in the "op" function as a parameter and does it on reals and imaginaries
+	private fun doOpOnBoth(other: ComplexNum, op: Double.(Double) -> Double): ComplexNum {
 		return ComplexNum(real.op(other.real), (imaginary.op(other.imaginary)))
 	}
 
-	operator fun minus(other: ComplexNum): ComplexNum{
+	operator fun minus(other: ComplexNum): ComplexNum {
 		return doOpOnBoth(other, (Double::minus))
 	}
 
-	operator fun plus(other: ComplexNum): ComplexNum{
+	operator fun plus(other: ComplexNum): ComplexNum {
 		return doOpOnBoth(other, (Double::plus))
 	}
     
-    operator fun times(other: ComplexNum): ComplexNum{
-        return ComplexNum(real * other.real - imaginary * other.imaginary, real * other.imaginary + imaginary * other.real )
+    operator fun times(other: ComplexNum): ComplexNum {
+        return ComplexNum(real * other.real - imaginary * other.imaginary, real * other.imaginary + imaginary * other.real)
 
 
     }
-    
-    operator fun div(other: ComplexNum): ComplexNum{
+   
+    operator fun div(other: ComplexNum): ComplexNum {
         val denom = other.real * other.real + other.imaginary * other.imaginary
         val realNum = real * other.real + imaginary * other.imaginary
         val imagNum = imaginary * other.real + real * other.imaginary
@@ -44,7 +47,7 @@ class ComplexNum(a: Double = 0.0){
 		return "" + real + " + " + imaginary + "i"
 	}
     
-    infix inline fun doIt(other: ComplexNum){
+    infix inline fun doIt(other: ComplexNum) {
 		println("" + this + " " + other)
 	}
 }

68ebccdbaf0b6efcd25945381c613df304754a7a
Nicholas P <zerrisk@gmail.com>
2017-05-17 21:51:24 -0700

    cleaned up code, removed non functional function

--- Complex Numbers/Main.kt
+++ Complex Numbers/Main.kt
@@ -1,61 +1,48 @@
-
-
 fun main(args: Array<String>) {
 	
-	/* TEST COMPLEX NUMBER CLASS */
-	println("COMPLEX NUMBER CLASS")
-	println("---------------------------")
-    var x = ComplexNum(2.0, -4.0)
-    var y = ComplexNum(4.0)
-    var z = ComplexNum(b=3.0)
-    println("(X + Z) -> " + (x + z))
+	println("Complex Numbers Test")
+	println("--------------------")
+	/* Code in comparison to Java's */
+	var first = ComplexNum(4.0, 5.0)
+	var second = ComplexNum(2.0, 1.0)
+	var third = first + second
+	println(third.toString())
+	
+	/*******************************/
+	
+	var x = ComplexNum(2.0, -4.0)
+	var y = ComplexNum(4.0)
+	var z = ComplexNum(b = 3.0)
+	
+	println("(X + Z) -> " + (x + z))
 	println("(X/Y + Z) -> " + (x/y + z))
+	println("--------------------\n")
 	
-	
-	/* TEST SMART CASTING */
-	println("\nSMART CASTING")
-	println("---------------------------")
-	print("Enter a string or a number: ")
-//	var userInput : String? = readLine()
-//	testSmartCast(userInput)
-	
-//	if (str is String) {
-//		var str2 : Double? = str.toDoubleOrNull()
-//		
-//		if (str2 is Double) {
-//			println(str2 + str2)
-//		}
-//	}
-	
-	
-	// null safety
-	
-
-	
-	
-    // NULL COALESCING
-	print("\nIf title is null, the supplied title is: ")
-	var suppliedTitle = null
-	val title = suppliedTitle ?: "Default title"
-	println(title)
-	
-	
-	
-//	println("Enter whatever: ")
-//	var isThisAString : String?
-//	isThisAString = null
-//	isThisAString = readLine()
-//	
-//	println(isThisAString)
-}
-
-// This function tests Smart Casting
-fun testSmartCast(anyVar : Any) {
-	if (anyVar is String) {
-		var anyVarDouble : Double? = anyVar.toDoubleOrNull()
-		
-		if (anyVarDouble is Double) {
-			println(anyVarDouble + anyVarDouble)
+	/* Other tests to show Kotlin's flexibility */
+	// Smart Casting
+	println("Smart Casting")
+	println("--------------------")
+	print("Enter a string or number: ")
+	var userInput = readLine()
+	if (userInput is String) {
+		var userInput2 = userInput.toDoubleOrNull()
+		if (userInput2 is Double) {
+			println("You entered a number and that number times 2 is: " + (userInput2 * 2))
+		}
+		else {
+			println("You entered a string saying: " + userInput)
 		}
 	}
+	println("--------------------")
+	
+	// Null Coalescing
+	println("Null Coalescing")
+	println("--------------------")
+	var a : String = "Hello"
+	var b : String? = "Hello"
+	// a = null (compilation error)
+	b = null
+	val greeting = b ?: "Default greeting"
+	println(greeting)
+	println("--------------------")
 }
